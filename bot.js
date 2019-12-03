315eedc74eacab8f91adddedd5515a2f65b25d84
FizzesShark <xbug2001@gmail.com>
2019-12-02 21:20:29 -0500

    First commit - no functions yet

--- bot.js
+++ bot.js
@@ -1,15 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var auth = require('./auth.json');
+const { token, prefix } = require('./config.json');
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+	if (!msg.content.startsWith(`${prefix}`) || msg.author.bot) return;
+
+	const args = msg.content.slice(prefix.length).split(/ +/);
+	const cmd = args.shift().toLowerCase();
+	if (cmd == 'args') {
+		msg.channel.send('Nice');
+	}
 });
 
-client.login(auth.token);
+client.login(token);
