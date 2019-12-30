1fff797eb28acd223258c237625a291182c73af2
FizzesShark <xbug2001@gmail.com>
2019-12-29 22:15:33 -0800

    Update train.js

--- commands/train.js
+++ commands/train.js
@@ -4,5 +4,16 @@ module.exports = {
 	usage: '<send-in-desired-channel>',
 	execute(msg) {
 		const channel = msg.channel;
+		
 	}
 };
+
+function getAll(channel, arr, latest) {
+	if (arr.length % 100 !== 0) {
+		return arr;
+	}
+	channel.fetchMessages({ before: latest.Snowflake }).then(messages => {
+		for (let i = 0; i < messages.length; i++) {
+			arr.push(messages[i].Message.content);
+	}});
+}
